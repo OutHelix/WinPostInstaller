66fd9a822aabc04b0610fda49304a22ce7a6837c
OutHelix <combo.contra@gmail.com>
2023-11-05 18:32:09 +0300

    add download_archive func and button_clicked func

--- src/main.py
+++ src/main.py
@@ -2,10 +2,12 @@ import time
 import zipfile
 import os
 import subprocess
+import requests
 
 CURRENT_PATH = os.getcwd()
-ARCHIVE_PATH = CURRENT_PATH + "/Downloads.zip"
+ARCHIVE_PATH = CURRENT_PATH + "/Downloads.py"
 EXTRACT_PATH = CURRENT_PATH + "/Downloads"
+ARCHIVE_URL = "https://mega.nz/file/eUMlHTQb#rCIdO_IHYqfjYjHXt0tTzsnyB5yoDjkSV4tHi3jm2Ec"
 
 APPLICATION = {
     "Discord": "DiscordSetup.exe",
@@ -26,14 +28,27 @@ APPLICATION = {
 }
 
 
-# Функция извлечения, для работы нужен архив!
+# функция для скачивания архива
+def download_archive(url, save_path):
+    response = requests.get(url)
+    with open(save_path, 'wb') as file:
+        file.write(response.content)
+    print("Архив успешно скачан!")
 
-# def button_clicked(selection):
-#     if selection:
-#         print(selection)
-#         with zipfile.ZipFile(ARCHIVE_PATH, "r") as archive:
-#             for app in selection:
-#                 if app in APPLICATION:
-#                     app_file = APPLICATION[app]
-#                     archive.extract(app_file, EXTRACT_PATH)
-#
+
+# Функция извлечения, для работы нужен архив!
+def button_clicked(selection):
+    # Проверяем наличие архива
+    if not os.path.exists(ARCHIVE_PATH):
+        download_archive(ARCHIVE_URL, ARCHIVE_PATH)
+
+    # Извлечение выбранных приложений из архива
+    with zipfile.ZipFile(ARCHIVE_PATH, "r") as archive:
+        for app in selection:
+            if app in APPLICATION:
+                app_file = APPLICATION[app]
+                archive.extract(app_file, EXTRACT_PATH)
+                print(f"Приложение {app} успешно извлечено!")
+
+
+download_archive(ARCHIVE_URL, ARCHIVE_PATH)
